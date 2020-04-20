d1aa1798b7123df434da21630b5cce1ae227d8d3
Christian Aichner <contact@aichner-christian.com>
2020-04-20 16:13:18 +0200

    Add ScrollToTop fix
    Our Scroll-To-Top fix has been implemented. This lays the 
    foundation for our static use of this ReactJS application.

---
+++ src/components/atoms/index.js
@@ -0,0 +1,12 @@
+//> Components
+// Import all components to export them for easy access from parent components
+import ScrollToTop from "./ScrollToTop";
+
+export {
+  ScrollToTop,
+};
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019-2020 Werbeagentur Christian Aichner
+ */
