4fa372517c4ba030133f9fae1f5a5d3e4b0fadf7
Aichnerc <contact@aichner-christian.com>
2020-04-25 22:06:18 +0200

    Add React-Helmet
    This allows us to set meta tags on each page.

--- src/components/pages/BrandingPage/index.jsx
+++ src/components/pages/BrandingPage/index.jsx
@@ -2,6 +2,10 @@
 // Contains all the functionality necessary to define React components
 import React from "react";
 
+//> Additional
+// React Helmet
+import { Helmet } from "react-helmet";
+
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
 import {
@@ -26,16 +30,19 @@ import { ReactComponent as Logo } from "../../../assets/content/logo_full.svg";
 import weddingsLogo from "../../../assets/content/weddings.png";
 
 class BrandingPage extends React.Component {
-	componentDidMount = () => {
-		// Set page title
-		document.title = "Evolving Our Identity";
-	};
-
 	render() {
 		const { globalProps } = this.props;
 
 		return (
 			<div id="branding" className={globalProps.darkMode ? "dark" : undefined}>
+				<Helmet>
+					<title>Evolving Our Identity</title>
+					<meta
+						name="description"
+						content="Du willst über uns berichten?
+						Nutze gerne unsere Ressourcen unter Berücksichtung der auf der Seite angeführten Regeln."
+					/>
+				</Helmet>
 				<MDBContainer className="py-5">
 					<h2 className="text-center mb-5">Logos und Verwendung</h2>
 					<MDBRow className="text-center">
--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -2,41 +2,49 @@
 // Contains all the functionality necessary to define React components
 import React from "react";
 
+//> Additional
+// React Helmet
+import { Helmet } from "react-helmet";
+
 //> Components
 // Sections
 import {
-  Hero,
-  ContactForm,
-  Services,
-  CallToAction,
-  Trusted,
+	Hero,
+	ContactForm,
+	Services,
+	CallToAction,
+	Trusted,
 } from "../../organisms/sections";
 
 //> CSS
 // All organisms use their own scss implementation
 
 class HomePage extends React.Component {
-  componentDidMount = () => {
-    document.title = "Ihr verlässlicher Allround-Partner";
-  };
+	render() {
+		const { globalProps } = this.props;
 
-  render() {
-    const { globalProps } = this.props;
-
-    if (globalProps) {
-      return (
-        <>
-          <Hero darkMode={globalProps.darkMode} />
-          <Services darkMode={globalProps.darkMode} />
-          <Trusted darkmode={globalProps.darkMode} />
-          <CallToAction darkMode={globalProps.darkMode} />
-          <ContactForm darkMode={globalProps.darkMode} />
-        </>
-      );
-    } else {
-      return null;
-    }
-  }
+		if (globalProps) {
+			return (
+				<>
+					<Helmet>
+						<title>Werbeagentur Christian Aichner - Ihr Partner</title>
+						<meta
+							name="description"
+							content="Unsere kostenlose Analyse zeigt Dir den aktuellen Stand Deiner gesamten Online-Präsenz und
+              liefert individuelle und unkomplizierte Lösungen."
+						/>
+					</Helmet>
+					<Hero darkMode={globalProps.darkMode} />
+					<Services darkMode={globalProps.darkMode} />
+					<Trusted darkmode={globalProps.darkMode} />
+					<CallToAction darkMode={globalProps.darkMode} />
+					<ContactForm darkMode={globalProps.darkMode} />
+				</>
+			);
+		} else {
+			return null;
+		}
+	}
 }
 
 export default HomePage;
