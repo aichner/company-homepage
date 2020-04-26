0dd062115e37b5fd28dbebf2f0434648263e3c91
Aichnerc <contact@aichner-christian.com>
2020-04-26 13:37:39 +0200

    Improve security
    The anonymous user has been added. The contact form can only be used, if there is an auth uid set.

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -6,48 +6,64 @@ import React from "react";
 // React Helmet
 import { Helmet } from "react-helmet";
 
+//> Redux
+// Connect
+import { connect } from "react-redux";
+// Actions
+import { signInAnonymous } from "../../../store/actions/authActions.js";
+
 //> Components
 // Sections
 import {
-  Hero,
-  ContactForm,
-  Services,
-  CallToAction,
-  Trusted,
+	Hero,
+	ContactForm,
+	Services,
+	CallToAction,
+	Trusted,
 } from "../../organisms/sections";
 
 //> CSS
 // All organisms use their own scss implementation
 
 class HomePage extends React.Component {
-  render() {
-    const { globalProps } = this.props;
+	componentDidMount = async () => {
+		console.log(await this.props.signInAnonymous());
+	};
 
-    if (globalProps) {
-      return (
-        <>
-          <Helmet>
-            <title>Werbeagentur Christian Aichner - Ihr Partner</title>
-            <meta
-              name="description"
-              content="Unsere kostenlose Analyse zeigt Dir den aktuellen Stand Deiner gesamten Online-Präsenz und
+	render() {
+		const { globalProps } = this.props;
+
+		if (globalProps) {
+			return (
+				<>
+					<Helmet>
+						<title>Werbeagentur Christian Aichner - Ihr Partner</title>
+						<meta
+							name="description"
+							content="Unsere kostenlose Analyse zeigt Dir den aktuellen Stand Deiner gesamten Online-Präsenz und
               liefert individuelle und unkomplizierte Lösungen."
-            />
-          </Helmet>
-          <Hero darkMode={globalProps.darkMode} />
-          <Services darkMode={globalProps.darkMode} />
-          <Trusted darkmode={globalProps.darkMode} />
-          <CallToAction darkMode={globalProps.darkMode} />
-          <ContactForm darkMode={globalProps.darkMode} />
-        </>
-      );
-    } else {
-      return null;
-    }
-  }
+						/>
+					</Helmet>
+					<Hero darkMode={globalProps.darkMode} />
+					<Services darkMode={globalProps.darkMode} />
+					<Trusted darkmode={globalProps.darkMode} />
+					<CallToAction darkMode={globalProps.darkMode} />
+					<ContactForm darkMode={globalProps.darkMode} />
+				</>
+			);
+		} else {
+			return null;
+		}
+	}
 }
 
-export default HomePage;
+const mapDispatchToProps = (dispatch) => {
+	return {
+		signInAnonymous: () => dispatch(signInAnonymous()),
+	};
+};
+
+export default connect(null, mapDispatchToProps)(HomePage);
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,58 +1,61 @@
 export const signIn = (credentials) => {
-  return (dispatch, getState, { getFirebase }) => {
-    const firebase = getFirebase();
+	return (dispatch, getState, { getFirebase }) => {
+		const firebase = getFirebase();
 
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(credentials.email, credentials.password)
-      .then(() => {
-        dispatch({
-          type: "LOGIN_SUCCESS",
-        });
-      })
-      .catch((err) => {
-        dispatch({
-          type: "LOGIN_ERROR",
-          err,
-        });
-      });
-  };
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(credentials.email, credentials.password)
+			.then(() => {
+				dispatch({
+					type: "LOGIN_SUCCESS",
+				});
+			})
+			.catch((err) => {
+				dispatch({
+					type: "LOGIN_ERROR",
+					err,
+				});
+			});
+	};
 };
 
-export const signInAnonymous = (credentials) => {
-  return (dispatch, getState, { getFirebase }) => {
-    const firebase = getFirebase();
+export const signInAnonymous = () => {
+	return async (dispatch, getState, { getFirebase }) => {
+    // Create Firebase instance
+		const firebase = getFirebase();
 
-    firebase
-      .auth()
-      .signInAnonymously()
-      .then(() => {
-        dispatch({
-          type: "LOGIN_ANON_SUCCESS",
-        });
-      })
-      .catch((err) => {
-        dispatch({
-          type: "LOGIN_ANON_ERROR",
-          err,
-        });
-      });
-  };
+    // Check if there is a user currently logged in
+		if (firebase.auth().currentUser) {
+			// There is a current user logged in
+			return true;
+		} else {
+			// No user logged in
+			return firebase
+				.auth()
+				.signInAnonymously()
+				.then(() => {
+					return true;
+				})
+				.catch((err) => {
+					return false;
+				});
+		}
+	};
 };
 
 export const signOut = () => {
-  return (dispatch, getState, { getFirebase }) => {
-    const firebase = getFirebase();
+	return (dispatch, getState, { getFirebase }) => {
+		const firebase = getFirebase();
 
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        dispatch({
-          type: "SIGNOUT_SUCCESS",
-        });
-      });
-  };
+		firebase
+			.auth()
+			.signOut()
+			.then(() => {
+				dispatch({
+					type: "SIGNOUT_SUCCESS",
+				});
+			});
+	};
 };
 
 /**
