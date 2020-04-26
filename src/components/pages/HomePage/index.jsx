7d25df64edc1e5e84bd6c95c680dd6cec653fab3
Aichnerc <contact@aichner-christian.com>
2020-04-26 14:38:55 +0200

    Fix indentation
    The indentation bug is really, really, really annoying.

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -15,48 +15,48 @@ import { signInAnonymous } from "../../../store/actions/authActions.js";
 //> Components
 // Sections
 import {
-	Hero,
-	ContactForm,
-	Services,
-	CallToAction,
-	Trusted,
+  Hero,
+  ContactForm,
+  Services,
+  CallToAction,
+  Trusted,
 } from "../../organisms/sections";
 
 //> CSS
 // All organisms use their own scss implementation
 
 class HomePage extends React.Component {
-	render() {
-		const { globalProps } = this.props;
+  render() {
+    const { globalProps } = this.props;
 
-		if (globalProps) {
-			return (
-				<>
-					<Helmet>
-						<title>Werbeagentur Christian Aichner - Ihr Partner</title>
-						<meta
-							name="description"
-							content="Unsere kostenlose Analyse zeigt Dir den aktuellen Stand Deiner gesamten Online-Präsenz und
+    if (globalProps) {
+      return (
+        <>
+          <Helmet>
+            <title>Werbeagentur Christian Aichner - Ihr Partner</title>
+            <meta
+              name="description"
+              content="Unsere kostenlose Analyse zeigt Dir den aktuellen Stand Deiner gesamten Online-Präsenz und
               liefert individuelle und unkomplizierte Lösungen."
-						/>
-					</Helmet>
-					<Hero darkMode={globalProps.darkMode} />
-					<Services darkMode={globalProps.darkMode} />
-					<Trusted darkmode={globalProps.darkMode} />
-					<CallToAction darkMode={globalProps.darkMode} />
-					<ContactForm darkMode={globalProps.darkMode} />
-				</>
-			);
-		} else {
-			return null;
-		}
-	}
+            />
+          </Helmet>
+          <Hero darkMode={globalProps.darkMode} />
+          <Services darkMode={globalProps.darkMode} />
+          <Trusted darkmode={globalProps.darkMode} />
+          <CallToAction darkMode={globalProps.darkMode} />
+          <ContactForm darkMode={globalProps.darkMode} />
+        </>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-	return {
-		signInAnonymous: () => dispatch(signInAnonymous()),
-	};
+  return {
+    signInAnonymous: () => dispatch(signInAnonymous()),
+  };
 };
 
 export default connect(null, mapDispatchToProps)(HomePage);
