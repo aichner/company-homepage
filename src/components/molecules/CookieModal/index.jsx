1b4f86f75eb4ad15e7c86ba6ac547af6fda5d76c
Christian Aichner <contact@aichner-christian.com>
2020-04-27 23:01:35 +0200

    Add Cookie modal
    The cookie modal has been added due to GDPR regulations.

---
+++ src/components/molecules/CookieModal/index.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import {
+  MDBContainer,
+  MDBBtn,
+  MDBModal,
+  MDBModalBody,
+  MDBIcon,
+  MDBRow,
+} from "mdbreact";
+
+class ModalPage extends Component {
+  state = {
+    modal1: localStorage.getItem("cookie") ? false : true,
+  };
+
+  toggle = (nr) => () => {
+    let modalNumber = "modal" + nr;
+    this.setState(
+      {
+        [modalNumber]: !this.state[modalNumber],
+      },
+      () => {
+        if (!localStorage.getItem("cookie")) {
+          localStorage.setItem("cookie", true);
+        }
+      }
+    );
+  };
+
+  render() {
+    return (
+      <MDBModal
+        isOpen={this.state.modal1}
+        toggle={this.toggle(1)}
+        frame
+        position="bottom"
+      >
+        <MDBModalBody className="text-center py-4">
+          <MDBRow className="justify-content-center align-items-center">
+            <p className="pt-3 pr-2 text-left">
+              Wir verwenden Cookies, um die Funktionsfähigkeit der Website
+              aufrecht zu erhalten.
+            </p>
+            <MDBBtn color="agency-red" onClick={this.toggle(1)} size="lg">
+              Ok, danke
+            </MDBBtn>
+          </MDBRow>
+        </MDBModalBody>
+      </MDBModal>
+    );
+  }
+}
+
+export default ModalPage;
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019-2020 Werbeagentur Christian Aichner
+ */
