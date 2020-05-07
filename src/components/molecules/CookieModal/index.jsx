2bc7c04a8c63e7a050ac5adb9a8b3f67d562d7d5
Luca Allmaier <allmaier@aichner.cloud>
2020-05-07 19:51:13 +0200

    Improve cookie modal
    The cookie modal has been enhanced in order to give the visitor more options. The layout has been updated too.

--- src/components/molecules/CookieModal/index.jsx
+++ src/components/molecules/CookieModal/index.jsx
@@ -11,12 +11,32 @@ import {
   MDBModalBody,
   MDBIcon,
   MDBRow,
+  MDBCol,
+  MDBInput,
 } from "mdbreact";
 
 class ModalPage extends React.Component {
   state = {
-    modal1: localStorage.getItem("cookie") ? false : true,
+    modal1: localStorage.getItem("essenziell") ? false : true,
+    essenziell: true,
+    statistiken: false,
+    marketing: false,
   };
+
+  checkBoxCheck(e) {
+    this.setState({ [e.target.name]: e.target.isChecked });
+  }
+
+  checkAll() {
+    this.setState({ essenziell: true, statistiken: true, marketing: true });
+  }
+
+  save() {
+    localStorage.setItem("essenziell", this.state.essenziell);
+    localStorage.setItem("statistiken", this.state.statistiken);
+    localStorage.setItem("marketing", this.state.marketing);
+    this.setState({ modal1: false });
+  }
 
   toggle = (nr) => () => {
     let modalNumber = "modal" + nr;
@@ -40,16 +60,70 @@ class ModalPage extends React.Component {
         toggle={this.toggle(1)}
         frame
         position="bottom"
+        backdrop="static"
+        keyboard={false}
       >
         <MDBModalBody className="text-center py-4">
           <MDBRow className="justify-content-center align-items-center">
-            <p className="pt-3 pr-2 text-left">
-              Wir verwenden Cookies, um die Funktionsfähigkeit der Website
-              aufrecht zu erhalten.
-            </p>
-            <MDBBtn color="agency-red" onClick={this.toggle(1)} size="lg">
-              Ok, danke
-            </MDBBtn>
+            <MDBCol>
+              <h2>Cookie Einstellungen</h2>
+              <p>
+                Wir verwenden auf unserer Website Cookies, um die
+                Benutzererfahrung zu optimieren. Einige davon sind essenziell.
+              </p>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="justify-content-center align-items-center">
+            <MDBCol className="col-4"></MDBCol>
+            <MDBCol className="col-2">
+              <MDBInput
+                label="Essenziell"
+                filled
+                checked={this.state.essenziell}
+                disabled
+                type="checkbox"
+                id="cookieEssenziell"
+                containerClass="mr-5"
+              />
+              <MDBInput
+                label="Marketing"
+                filled
+                checked={this.state.marketing}
+                name="marketing"
+                color="primary"
+                type="checkbox"
+                id="cookieMarketing"
+                containerClass="mr-5"
+                onClick={(e) => this.checkBoxCheck(e)}
+              />
+            </MDBCol>
+            <MDBCol className="col-2">
+              <MDBInput
+                label="Statistiken"
+                filled
+                checked={this.state.statistiken}
+                name="statistiken"
+                type="checkbox"
+                id="cookieStatistiken"
+                containerClass="mr-5"
+                onClick={(e) => this.checkBoxCheck(e)}
+              />
+            </MDBCol>
+            <MDBCol className="col-4"></MDBCol>
+          </MDBRow>
+          <MDBRow className="justify-content-center align-items-center">
+            <MDBCol>
+              <MDBBtn color="success" onClick={() => this.checkAll()}>
+                Alle akzeptieren
+              </MDBBtn>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="justify-content-center align-items-center">
+            <MDBCol>
+              <MDBBtn color="" onClick={() => this.save()}>
+                Speichern
+              </MDBBtn>
+            </MDBCol>
           </MDBRow>
         </MDBModalBody>
       </MDBModal>
