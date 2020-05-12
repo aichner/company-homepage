f2d2adbf9b4549de5e97dd5db1201d3d426a1bbb
Aichnerc <contact@aichner-christian.com>
2020-05-12 14:17:52 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6a80860b02b1ddd63d6f01795fafa2f1.js"
+  "./precache-manifest.c8627938b6b9fcf45155446993315aa5.js"
 );
 
 self.addEventListener('message', (event) => {
